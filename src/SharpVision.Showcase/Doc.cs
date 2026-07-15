78bf9a89680bb5c1bd02ff6e4af9e0aa8a29b506
Alexandru Ciobanu <alex+git@ciobanu.org>
2026-07-15 11:45:48 +0100

    chore: refactor

--- src/SharpVision.Showcase/Doc.cs
+++ src/SharpVision.Showcase/Doc.cs
@@ -1,7 +1,7 @@
 // Copyright (c) SharpVision contributors. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-namespace SharpVision.Showcase.Panes;
+namespace SharpVision.Showcase;
 
 using SharpVision.Text;
 
